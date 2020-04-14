3835d0fb92416ebd74d2955fd0e135d1aeecbb4d
pengxuem <903322147@qq.com>
2020-04-14 22:07:14 +0800

    two

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 var express = require('express')
 var path = require('path')
 var bodyParser = require('body-parser')
@@ -5,15 +6,58 @@ var bodyParser = require('body-parser')
 
 var app = express()
 
-
+// 暴露公共资源
 app.use('/public/', express.static(path.join(__dirname, './public')))
 app.use('/node_modules/', express.static(path.join(__dirname, './node_modules')))
+app.use('/views/', express.static(path.join(__dirname, './views')))
+
+
+// 获取post请的参数
+app.use(bodyParser.urlencoded({extenden: false}))
+app.use(bodyParser.json())
+
+// art模板
+app.engine('html', require('express-art-template'))
 
 
 app.get('', function(req, res){
     res.send('hello!')
 })
 
+
 app.listen(3000, function(){
     console.log('express 3000 app is running...')
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
