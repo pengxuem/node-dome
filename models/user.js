454d038b9588fda3c929b99664e2c5fd7d9cc677
pengxuem <903322147@qq.com>
2020-04-17 21:54:02 +0800

    q

---
+++ models/user.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose')
+
+var Schema = mongoose.Schema
+
+
+var userSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    nickname: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    create_time: {
+        type: Date,
+        default: Date.now
+    }
+
+})
+
+module.exports = mongoose.model('User', userSchema)
--- router/router.js
+++ router/router.js
@@ -4,20 +4,33 @@ var router = express.Router()
 
 // get方法请求首页
 router.get('/', function(req, res){
-    res.render('index.html', {
-        data: 'hello!'
-    })
+    res.render('index.html')
 })
 
-
+// 发表页面
 router.get('/topics/new', function(req, res){
     res.render('login.html')
 })
+
+// 登录页面
 router.get('/login', function(req, res){
     res.render('login.html')
 })
+
+// 注册页面
 router.get('/register', function(req, res){
     res.render('register.html')
+})
+
+// 用户登录
+router.post('/login', function(req, res){
+    console.log(req.body)
+    
+})
+
+// 用户注册
+router.post('/register', function(req, res){
+    console.log(req.body)
 })
 
 
